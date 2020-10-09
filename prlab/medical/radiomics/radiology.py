fe039a36f8a36c347f11db76c98dbd03ea2f4b8f
hung <thanhhungqb@gmail.com>
2020-10-09 21:55:58 +0900

    fix bug: should expand_dims to get [1, z, y, x]; customizable map_name_fn

--- prlab/medical/radiomics/radiology.py
+++ prlab/medical/radiomics/radiology.py
@@ -11,7 +11,7 @@ class SliceDataset(Dataset):
     Work with pytorch
     """
 
-    def __init__(self, df, path, transform=None, **config):
+    def __init__(self, df, path, transform=None, map_name_fn=None, **config):
         """
         data: extracted npy data with shape (slice_count x H_size x W_size
         NOTE: df should be .reset_index(drop=True) to make [0, len) otherwise access by index will be wrong
@@ -21,7 +21,7 @@ class SliceDataset(Dataset):
         self.df = df
         self.path = Path(path)
         self.transform = transform
-        self.map_name_fn = lambda pid: f"{pid}.npy"
+        self.map_name_fn = map_name_fn if map_name_fn is not None else (lambda pid: f"{pid}.npy")
 
     def __getitem__(self, index):
         data_path = self.path / self.map_name_fn(self.df['pid'][index])
@@ -30,6 +30,7 @@ class SliceDataset(Dataset):
         if self.transform:
             slices = self.transform(slices)
 
+        slices = np.expand_dims(slices, axis=0).astype(np.float32)
         return slices, self.df['Survival.time'][index]
 
     def __len__(self):
