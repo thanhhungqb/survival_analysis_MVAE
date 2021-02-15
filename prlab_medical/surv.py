066ce1724e2dae87b173d9e97365400e06da7fc4
Hung Vo <thanhhungqb@gmail.com>
2021-02-15 16:45:54 +0900

    loss combine hazard and mse for individual survival predict

--- prlab_medical/surv.py
+++ prlab_medical/surv.py
@@ -24,10 +24,11 @@ class LossLogHazInd(nn.Module):
     Task Haz + Individual reg, size (T+1)
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, alpha=0.5, **kwargs):
         super().__init__()
         self.loss_surv = NLLLogistiHazardLoss()
         self.loss_ind = nn.MSELoss()
+        self.alpha = alpha
 
     def forward(self, phi, target_loghaz):
         phi, ind_sv = phi[:, :-1], phi[:, -1]
@@ -38,7 +39,17 @@ class LossLogHazInd(nn.Module):
 
         loss_surv = self.loss_surv(phi, idx_durations, events)
 
-        return loss_surv
+        # mse only for event
+        with torch.no_grad():
+            n_sample = phi.size()[0]
+            device = phi.device
+            zeros = torch.tensor([0.] * n_sample).to(device)
+            ones = torch.tensor([1.] * n_sample).to(device)
+            hs = torch.where(t_org_event > 0, ones, zeros)
+        se = (ind_sv - t_ind_sv) * (ind_sv - t_ind_sv)
+        loss_mse = torch.sum(se * hs) / torch.sum(hs)
+
+        return loss_surv * self.alpha + loss_mse * (1 - self.alpha)
 
 
 class LabelTransform:
@@ -73,3 +84,13 @@ def make_label_transform_pipe(**config):
 
     config['labtrans_cuts'] = label_trans.trans.idu.cuts
     return config
+
+
+# surv post process for predict, see input form `prlab.torch.utils.default_post_process_fn`
+def surv_ppp_merge_hazard_st_fn(batch_tensor, **config):
+    """
+    output from MultiDecoderVAE with only one second_decoder
+    [[bs, n_hazard+1]]"""
+    x = batch_tensor[0].cpu().detach().numpy().tolist()
+
+    return x
