d5c47f613294cd0432120b7d94ca9332ea552591
Hung Vo <thanhhungqb@gmail.com>
2021-02-15 18:00:22 +0900

    add report function and pipe

--- prlab_medical/surv.py
+++ prlab_medical/surv.py
@@ -1,10 +1,15 @@
 """
 Implement some function related to survival analysis
 """
+import math
+
+import pandas as pd
 import torch
 import torch.nn as nn
 from pycox.models.loss import NLLLogistiHazardLoss
 from pycox.preprocessing.label_transforms import LabTransDiscreteTime
+
+from prlab_medical.data_loader import event_norm
 
 
 class LossAELogHaz(nn.Module):
@@ -101,5 +106,76 @@ def surv_ppp_merge_hazard_st_fn(batch_tensor, **config):
         return {'hazard': one[0], 'survival time': one[1]}
 
     ret = [fn(one) for one in x]
+
+    return ret
+
+
+# report for survival
+def report_survival_time(**config):
+    # run after make prediction and save to file, e.g. predict.csv
+    # compare with test.csv, metric maybe MAE, CI, ...
+    # column name should be fixed (customable later)
+    test_filename, predict_filename = 'test.csv', 'predict.csv'
+    pid_name = 'pid'
+    predict_header_names = ['hazard', 'survival time', 'pid']
+
+    gt = pd.read_csv(config['cp'] / test_filename)
+    pred = pd.read_csv(config['cp'] / predict_filename)
+
+    gt = gt[config['y_names'] + [pid_name]]
+    pred = pred[predict_header_names]
+
+    gt = gt.rename(columns={config['y_names'][0]: 'gt_sv', config['y_names'][1]: 'event', 'pid': pid_name})
+    pred = pred.rename(
+        columns={predict_header_names[0]: 'pred_hazard', predict_header_names[1]: 'pred_sv', 'pid': pid_name})
+
+    # merge two df to one by pid
+    merge_df = pd.concat([gt, pred], axis=1, join='inner')
+    merge_df['event'] = event_norm(merge_df['event'])
+
+    try:
+        merge_df.to_csv(config['cp'] / 'merge_gt_pred.csv')
+    except:
+        pass
+
+    config['out'] = {
+        'MAE': mae_non_censoring_only(merge_df),
+        'MSE': mse_non_censoring_only(merge_df),
+        'default_mae_mse': default_mae_mse(merge_df),
+        'CI': None
+    }
+
+    return config
+
+
+def mae_non_censoring_only(df):
+    """
+    dataframe with ground truth and predict with fixed names
+    """
+    ae = abs(df['gt_sv'] - df['pred_sv'])
+    return sum(ae * df['event']) / sum(df['event'])
+
+
+def mse_non_censoring_only(df):
+    """
+    dataframe with ground truth and predict with fixed names
+    """
+    se = (df['gt_sv'] - df['pred_sv']) * (df['gt_sv'] - df['pred_sv'])
+    ret = sum(se * df['event']) / sum(df['event'])
+    ret = math.sqrt(ret)
+    return ret
+
+
+def default_mae_mse(df):
+    mean = sum(df['gt_sv'] * df['event']) / sum(df['event'])
+
+    # mse
+    se = (df['gt_sv'] - mean) * (df['gt_sv'] - mean)
+    mse = sum(se * df['event']) / sum(df['event'])
+
+    # mae
+    ae = abs(df['gt_sv'] - mean)
+
+    ret = sum(ae * df['event']) / sum(df['event']), math.sqrt(mse)
 
     return ret
